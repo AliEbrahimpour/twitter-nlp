1837eecce09d6ef10fc291c867d62c44f98f8426
Ali_Ebrahimpour <ali74.ebrahimpour@yahoo.com>
2018-04-14 09:05:11 +0430

    to step 6

--- twitter-step-6.py
+++ twitter-step-6.py
@@ -4,6 +4,7 @@ n=input("enter word: ")
 print(n)
 line=""
 t=""
+
 with codecs.open("step5/inverted-index.txt", 'r', encoding="utf-8") as f:
     for text in f.readlines():
         for i in range(0, len(text)):
